81ea62a4357eac7d95f7503105493b48446a7fad
John Stringer <stringerjohn@outlook.com>
2022-12-08 09:35:16 +0800

    Initial Commit

---
+++ GitExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by John Stringer on 8/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
